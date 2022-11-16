71c1823ff8f5b32d211d36dc842eafbb14aa7233
palminski <williambolls98@gmail.com>
2022-11-16 09:55:52 -0800

    added about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,31 @@
 function About() {
     
     return(
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <h1 className="my-4">About Me</h1>
+            <hr/>
+            <div className="container my-5">
+                <div className="row justify-content-between">
+                    <img src={require("../../assets/images/monkey-at-computer.jpg")} alt="William Bolls" className="col-4 portrait"></img>
+                    <p className="col-7">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </div>
+                <div className="row mt-5">
+                    <h2 className="col-12">Skills</h2>
+                </div>
+                <hr></hr>
+                <div className="row mt-5 justify-content-between">
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+                    <img className="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                </div>  
+            </div>
+        </>
     );
 }
 
