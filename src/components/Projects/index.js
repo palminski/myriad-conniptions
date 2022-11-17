1c70318e3f3d5f23c2a88532773bb62d5c100d7c
palminski <williambolls98@gmail.com>
2022-11-16 20:50:50 -0800

    added additional projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,43 +17,49 @@ function Projects() {
             title: "The Lair of the Sphinx",
             imageLocation: require("../../assets/images/project-images/sphinx-page.png"),
             imageAlt: "the fearsome sphinx",
-            description: "Lorem Ipsum",
-            link: "https://the-lair-of-the-sphinx.herokuapp.com/"
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            link: "https://the-lair-of-the-sphinx.herokuapp.com/",
+            repoLink: "https://github.com/Dodgemech/The-Lair-of-the-Sphinx"
         },
         {
             title: "Ultimate Brew Master",
             imageLocation: require("../../assets/images/project-images/brew-master-page.png"),
             imageAlt: "beer and keg",
-            description: "Lorem Ipsum",
-            link: "https://palminski.github.io/ultimate-brew-master-brewery-finder/"
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            link: "https://palminski.github.io/ultimate-brew-master-brewery-finder/",
+            repoLink: "https://github.com/palminski/ultimate-brew-master-brewery-finder"
         },
         {
-            title: "Ultimate Brew Master",
-            imageLocation: require("../../assets/images/project-images/brew-master-page.png"),
-            imageAlt: "beer and keg",
-            description: "Lorem Ipsum",
-            link: "https://palminski.github.io/ultimate-brew-master-brewery-finder/"
+            title: "Ra-byte Blog",
+            imageLocation: require("../../assets/images/project-images/blog-page.PNG"),
+            imageAlt: "picture of a simple website",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            link: "https://shrouded-island-63424.herokuapp.com/",
+            repoLink: "https://github.com/palminski/rabyte-blog"
         },
         {
-            title: "Ultimate Brew Master",
-            imageLocation: require("../../assets/images/project-images/brew-master-page.png"),
-            imageAlt: "beer and keg",
-            description: "Lorem Ipsum",
-            link: "https://palminski.github.io/ultimate-brew-master-brewery-finder/"
+            title: "Cloud Nine",
+            imageLocation: require("../../assets/images/project-images/weather-page.png"),
+            imageAlt: "weather forecast website",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            link: "https://palminski.github.io/cloud-nine-weather-app/",
+            repoLink: "https://github.com/palminski/cloud-nine-weather-app"
         },
         {
-            title: "Ultimate Brew Master",
-            imageLocation: require("../../assets/images/project-images/brew-master-page.png"),
-            imageAlt: "beer and keg",
-            description: "Lorem Ipsum",
-            link: "https://palminski.github.io/ultimate-brew-master-brewery-finder/"
+            title: "Employee Manager",
+            imageLocation: require("../../assets/images/project-images/manager-page.JPG"),
+            imageAlt: "Comand Line Interface employee managing app",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            link: "https://drive.google.com/file/d/1U-wY1mN58ghzcpkD220CrHqd9MdBbOKN/view",
+            repoLink: "https://github.com/palminski/employee-manager-rabit"
         },
         {
-            title: "Ultimate Brew Master",
-            imageLocation: require("../../assets/images/project-images/brew-master-page.png"),
-            imageAlt: "beer and keg",
-            description: "Lorem Ipsum",
-            link: "https://palminski.github.io/ultimate-brew-master-brewery-finder/"
+            title: "Note Taker",
+            imageLocation: require("../../assets/images/project-images/note-page.PNG"),
+            imageAlt: "Comand Line Interface employee managing app",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            link: "https://express-note-taker-rabit.herokuapp.com/notes",
+            repoLink: "https://github.com/palminski/express-note-taker-rabit"
         },
     ];
     
