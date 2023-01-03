35ad1cd7ca3e85b17426ddc6fde6c8eb1660f75f
palminski <williambolls98@gmail.com>
2023-01-03 15:20:44 -0800

    adjusted text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {useState} from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Projects from './components/Projects';
-import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
