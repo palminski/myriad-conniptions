3259a51a8f9b5d0d58375d11c6bf2077b283fc31
palminski <williambolls98@gmail.com>
2023-01-25 12:04:13 -0800

    imporoved mobile a bit

--- src/components/DetailsModal/index.js
+++ src/components/DetailsModal/index.js
@@ -25,7 +25,10 @@ function DetailsModal({currentProject, onClose}) {
                     {currentProject.link && <a className="col-7 col-md-5 btn btn-primary my-2" href={currentProject.link} target="_blank">View Webpage</a>}
                     {currentProject.repoLink && <a className="col-7 col-md-5 btn btn-primary my-2" href={currentProject.repoLink} target="_blank">View Repo <i class="devicon-github-original"></i></a>}
                     {currentProject.youtubeLink && <a className="col-7 col-md-5 btn btn-primary my-2" href={currentProject.youtubeLink} target="_blank">View Video</a>}
+                    
+                    
                     </div>
+                    <button className="col-7 col-md-5 btn btn-danger my-2" onClick={onClose}>Close</button>
 
                 </div>
             </div>
