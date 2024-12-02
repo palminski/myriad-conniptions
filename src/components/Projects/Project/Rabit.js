daf85b992457813defaaacc224f42783100d832a
Will <williambolls98@gmail.com>
2024-12-01 18:51:23 -0800

    Updated rabit page

--- src/components/Projects/Project/Rabit.js
+++ src/components/Projects/Project/Rabit.js
@@ -6,24 +6,59 @@ function Rabit() {
                 <h1 className="my-4 ">RaBit</h1>
 
                 <div className="container grow-in px-4">
-                    <div className="row justify-content-around justify-content-lg-between bg-c-light-brown  project-container">
-                        <img src={require("../../../assets/images/project-images/rabit-image.png")} alt="A Cute Pixel Art Bunny" className="drop-shadow img-fluid col-12 col-lg-4 project-image  "></img>
-                        <p className="col-12 col-lg-8 text-white text-align-left project-text">
-                            A game I have been working on in between other projects. Previously I was making it using GameMaker and Game Maker Language (GML). Recently I have switched to developing in Unity using C# as it is a much more powerful engine.
+                    <div className=" bg-c-light-brown  project-container clearfix pt-3">
+                        <img src={require("../../../assets/images/project-images/RABIT_IMAGE.png")} alt="William Bolls" className="drop-shadow img-fluid portrait px-4"></img>
+                        <p className="text-white text-align-left px-4 pb-4">
+                            A game I have been creating on and off over the last few years. Previously I was making it using GameMaker Studio, I have since switched to developing in Unity.
                             <br />
-                            
-                            Below you can access a YouTube playlist documenting the development of the game when it was being made in GameMaker, as well as download a zip folder containing the most recent build
+
+                            If you would like to play test it I would love to hear your feedback! You can download an alpha build here or email me to request one. Keep in mind it is still in relatively rough shape, but it is totally playable!
                             <br />
-                            <br/>
-                            Controls For Controller: <br></br>Left Joystick to move, South Button to jump, and Right Trigger to shift.
                             <br />
-                            
-                            Controls For Keyboard: <br></br>WASD or Arrows to move and jump, Space to shift.
+                            If you are helping me test please try out the testing gauntlet and rate each levels difficulty and how much you enjoyed it on a scale from 1 to 10. Additionally, if you have any notes on a specific room that would be super helpful as well. A lot of rooms are very difficult as, unfortunately I have become very skilled at my own game and it has been kind of hard for me to gauge what will be doable for the average or newcomer player. If you see a level that has a cool idea in it but seems too difficult, let me know so I can include an easier version of the same idea earlier in the game.
+                            <br></br>
+                            Some Levels are VERY hard and levels are in no particular order. While in the gauntlet you can press O and P on your keyboard to go forward and backwards through levels to skip overly challenging ones or return to a specific room.
+                            <br />
+                            <br />
+                            <strong>
+                                Controls For Controller:
+                            </strong>
+                            <br></br>
+                            Left Joystick to move, South Button to jump, and Right Trigger to shift.
+                            <br></br>
+                            East Button to dash, east button and down on joystick while in air to ground pound.
+                            <br></br>
+                            Start Button to bring up menu
+                            <br />
+                            <br></br>
+                            <strong>
+                                Controls For Keyboard:
+                            </strong>
+                            <br></br>
+                            WASD or Arrows to move and jump
+                            <br></br>
+                            Space, Z, or N to shift.
+                            <br></br>
+                            X, or M to Dash.
+                            <br></br>
+                            Down Arrow or S while in air to ground pound
+                            <br></br>
+                            Escape to bring up menu
+                            <strong>
+                                <br></br>
+                                <br></br>
+                                NOTE:
+                            </strong>
+                            <br></br>
+                            Main menu and pause menu can not be interacted with by mouse. A rework for it is on my list of future updates. Currently you can navigate it using the arrows/joystick and South Button or Enter..
+
                         </p>
                     </div>
-                    <div className="row  justify-content-left drop-shadow">
-                        <a className="project-tab  mr-2" href={require("../../../assets/downloadable-files/RABIT.zip")} download>Download Build</a>
-                        <a className="project-tab  mr-2" href={"https://www.youtube.com/playlist?list=PLxVRBSIe4S9E1RD_C8kEtvJ5VGjsP85WM"} target="_blank">View Old Videos</a>
+
+                    
+                    <div className="row ml-0 justify-content-left drop-shadow">
+                        <a className="project-tab  mr-2" href={require("../../../assets/downloadable-files/RABIT_alpha.zip")} download>Download Alpha</a>
+                        <a className="project-tab  mr-2" href={"https://www.youtube.com/playlist?list=PLxVRBSIe4S9E1RD_C8kEtvJ5VGjsP85WM"} >View Old Videos</a>
                     </div>
 
                     <div className="row mt-5 justify-content-center">
